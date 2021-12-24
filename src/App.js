c43c0fe14ee45dda2810277ca10dee36b71e80f8
poojitha8812 <poojithanagraj8812@gmail.com>
2021-12-24 15:17:19 +0530

    breakinbad message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [data ,setData] =useState('')
+  
+const getAllData = () => {
+  
+        axios
+          .get("https://www.breakingbadapi.com/api/characters/")
+          .then((response) => {
+            console.log(response.data);
+            setData(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
 }
+useEffect(() => {
+      getAllData();
+},[]);
+
+return (
+        <>
+            {data ? 
+                data.map(data => {
+                    return(
+                      
+                      
+
+                    <div className="mar" style={{display:"inline-block"}}>
+                      
+                       <div className="data App " key={data.id} >
+                         
+                         <h3 style={{color:"white"}}>{data.name}</h3>
+                         <span><img src={data.img} alt="none" style={{height:"200px", width:"200px"}} /></span>
+                         <h4 style={{color:"white"}}>{data.birthday}</h4>
+                         
+                       </div>
+                       </div>
+                       
+
+
+                       
+                    )
+                     }) : <h3>No data yet</h3> }
+        </>
+    )
+}
+
+
+
+
+
+ 
+  
 
 export default App;
